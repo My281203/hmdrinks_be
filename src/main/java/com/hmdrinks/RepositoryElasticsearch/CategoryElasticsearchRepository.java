d1d744e7c94581fef77e5af274f3f0d5890c43e1
DiemMy <125851029+My281203@users.noreply.github.com>
2025-06-09 23:25:41 +0700

    Update CategoryElasticsearchRepository.java

--- src/main/java/com/hmdrinks/RepositoryElasticsearch/CategoryElasticsearchRepository.java
+++ src/main/java/com/hmdrinks/RepositoryElasticsearch/CategoryElasticsearchRepository.java
@@ -6,6 +6,7 @@ import com.hmdrinks.Entity.Product;
 import com.hmdrinks.Entity.ProductElasticsearch;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.repository.ReactiveElasticsearchRepository;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -13,7 +14,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface CategoryElasticsearchRepository extends ElasticsearchRepository<CategoryElasticsearch, String> {
+public interface CategoryElasticsearchRepository extends ReactiveElasticsearchRepository<CategoryElasticsearch, String> {
 
 
 
